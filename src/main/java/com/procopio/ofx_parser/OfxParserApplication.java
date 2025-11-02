254c5cf7e2f93af7ea364442079571046a904517
luis_procopio <luisprocopioarruda@gmail.com>
2025-11-01 23:36:36 -0300

    Repository init
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/procopio/ofx_parser/OfxParserApplication.java
@@ -0,0 +1,13 @@
+package com.procopio.ofx_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfxParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfxParserApplication.class, args);
+	}
+
+}
